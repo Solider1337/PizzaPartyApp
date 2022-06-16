5fca56cb91ecfeab92f0470578a77de0130364bb
Parabeac-Nest <admin@parabeac.com>
2022-06-16 23:24:53 +0000

    Auto-generated commit by Parabeac Nest

---
+++ lib/views/page_1/size.g.dart
@@ -0,0 +1,459 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Size extends StatefulWidget {
+  const Size({
+    Key? key,
+  }) : super(key: key);
+  @override
+  _Size createState() => _Size();
+}
+
+class _Size extends State<Size> {
+  _Size();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Color(0xffffd951),
+      child: Stack(children: [
+        Positioned(
+          left: 206.0,
+          width: 464.0,
+          top: 14.0,
+          height: 379.0,
+          child: Image.asset(
+            'assets/images/3786128removebgpreview1.png',
+            package: 'pizzaparty',
+            width: 464.000,
+            height: 379.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 0,
+          width: 784.0,
+          top: 362.0,
+          height: 595.0,
+          child: Container(
+            width: 784.000,
+            height: 595.000,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(50)),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 315.0,
+          width: 171.0,
+          top: 644.0,
+          height: 70.0,
+          child: Container(
+              width: 171.000,
+              height: 70.000,
+              child: AutoSizeText(
+                '9',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 48,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+        Positioned(
+          left: 407.0,
+          width: 55.0,
+          top: 550.0,
+          height: 55.0,
+          child: Image.asset(
+            'assets/images/ellipse6.png',
+            package: 'pizzaparty',
+            width: 55.000,
+            height: 55.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 344.0,
+          width: 55.0,
+          top: 550.0,
+          height: 55.0,
+          child: Image.asset(
+            'assets/images/ellipse5.png',
+            package: 'pizzaparty',
+            width: 55.000,
+            height: 55.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 281.0,
+          width: 55.0,
+          top: 550.0,
+          height: 55.0,
+          child: Image.asset(
+            'assets/images/ellipse4.png',
+            package: 'pizzaparty',
+            width: 55.000,
+            height: 55.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 470.0,
+          width: 55.0,
+          top: 550.0,
+          height: 55.0,
+          child: Image.asset(
+            'assets/images/ellipse7.png',
+            package: 'pizzaparty',
+            width: 55.000,
+            height: 55.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 525.0,
+          width: 64.0,
+          top: 39.0,
+          height: 64.0,
+          child: Container(
+              clipBehavior: Clip.hardEdge,
+              width: 64.000,
+              height: 64.000,
+              decoration: BoxDecoration(),
+              child: Stack(children: [
+                Positioned(
+                  left: 1.964,
+                  width: 60.1,
+                  top: 7.596,
+                  height: 48.732,
+                  child: SvgPicture.asset(
+                    'assets/images/group.svg',
+                    package: 'pizzaparty',
+                    width: 60.100,
+                    height: 48.732,
+                    fit: BoxFit.none,
+                  ),
+                ),
+              ])),
+        ),
+        Positioned(
+          left: 222.0,
+          width: 174.0,
+          top: 39.0,
+          height: 108.0,
+          child: Container(
+              width: 174.000,
+              height: 108.000,
+              child: AutoSizeText(
+                'Pizza Party',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 36,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 206.0,
+          width: 368.0,
+          top: 406.0,
+          height: 75.0,
+          child: Container(
+              width: 368.000,
+              height: 75.000,
+              child: AutoSizeText(
+                'How many pieces does pizza have?',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 28,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: 0,
+                  color: Color(0xff464646),
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+        Positioned(
+          left: 249.0,
+          width: 290.0,
+          top: 741.0,
+          height: 70.0,
+          child: Container(
+            width: 290.000,
+            height: 70.000,
+            decoration: BoxDecoration(
+              color: Color(0xffffd951),
+              borderRadius: BorderRadius.all(Radius.circular(50)),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 260.0,
+          width: 55.0,
+          top: 748.0,
+          height: 55.0,
+          child: Image.asset(
+            'assets/images/ellipse1.png',
+            package: 'pizzaparty',
+            width: 55.000,
+            height: 55.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 291.0,
+          width: 1.0,
+          top: 776.0,
+          height: 3.0,
+          child: SvgPicture.asset(
+            'assets/images/arrow1.svg',
+            package: 'pizzaparty',
+            width: 1.000,
+            height: 3.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 344.0,
+          width: 113.0,
+          top: 758.0,
+          height: 36.0,
+          child: Container(
+              width: 113.000,
+              height: 36.000,
+              child: AutoSizeText(
+                'Continue',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+        Positioned(
+          left: 319.0,
+          width: 150.0,
+          top: 877.0,
+          height: 8.0,
+          child: SvgPicture.asset(
+            'assets/images/line1.svg',
+            package: 'pizzaparty',
+            width: 150.000,
+            height: 8.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 249.0,
+          width: 290.0,
+          top: 644.0,
+          height: 70.0,
+          child: Container(
+            width: 290.000,
+            height: 70.000,
+            decoration: BoxDecoration(
+              color: Color(0x78ffcd1d),
+              borderRadius: BorderRadius.all(Radius.circular(50)),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 287.0,
+          width: 42.0,
+          top: 538.0,
+          height: 58.0,
+          child: Container(
+              width: 42.000,
+              height: 58.000,
+              child: AutoSizeText(
+                's',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 48,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+        Positioned(
+          left: 413.0,
+          width: 42.0,
+          top: 547.0,
+          height: 58.0,
+          child: Container(
+              width: 42.000,
+              height: 58.000,
+              child: AutoSizeText(
+                'L',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 40,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+        Positioned(
+          left: 477.0,
+          width: 42.0,
+          top: 552.0,
+          height: 58.0,
+          child: Container(
+              width: 42.000,
+              height: 58.000,
+              child: AutoSizeText(
+                'XL',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 34,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+        Positioned(
+          left: 350.0,
+          width: 42.0,
+          top: 547.0,
+          height: 58.0,
+          child: Container(
+              width: 42.000,
+              height: 58.000,
+              child: AutoSizeText(
+                'M',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 40,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+        Positioned(
+          left: 470.0,
+          width: 55.0,
+          top: 651.0,
+          height: 55.0,
+          child: Image.asset(
+            'assets/images/ellipse2.png',
+            package: 'pizzaparty',
+            width: 55.000,
+            height: 55.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 260.0,
+          width: 55.0,
+          top: 651.0,
+          height: 55.0,
+          child: Image.asset(
+            'assets/images/ellipse3.png',
+            package: 'pizzaparty',
+            width: 55.000,
+            height: 55.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 503.0,
+          width: 1.0,
+          top: 679.0,
+          height: 3.0,
+          child: SvgPicture.asset(
+            'assets/images/arrow2.svg',
+            package: 'pizzaparty',
+            width: 1.000,
+            height: 3.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 280.0,
+          width: 2.0,
+          top: 679.0,
+          height: 3.0,
+          child: SvgPicture.asset(
+            'assets/images/arrow3.svg',
+            package: 'pizzaparty',
+            width: 2.000,
+            height: 3.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 258.0,
+          width: 290.0,
+          top: 520.0,
+          height: 27.0,
+          child: Container(
+              width: 290.000,
+              height: 27.000,
+              child: AutoSizeText(
+                'Popular size templates',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 0,
+                  color: Color(0xff949494),
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+        Positioned(
+          left: 309.0,
+          width: 171.0,
+          top: 644.0,
+          height: 70.0,
+          child: Container(
+              width: 171.000,
+              height: 70.000,
+              child: AutoSizeText(
+                '8',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 48,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+      ]),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
